8d78b516f36ff71fce4cd8a392e77dbb7ef7f868
가현 <102145605+rkgus24@users.noreply.github.com>
2022-10-01 15:38:01 +0900

    Update Chapter19.py

--- Chapter19.py
+++ Chapter19.py
@@ -1,23 +1,74 @@
-# UDPEchoServer
-from socket import *
+    <%@page import="DBPKG.Util"%>
+    <%@page import="java.sql.*"%>
+    <%@ page language="java" contentType="text/html; charset=UTF-8"
+        pageEncoding="UTF-8"%>
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="UTF-8">
+    <title>list</title>
+    <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <jsp:include page="header.jsp"/>
 
-s_ip = 'localhost'
-s_port = 9999
-s_addr = (s_ip, s_port)
+    <section>
+    <h2><b>회원목록조회/수정</b></h2><br>
+    <form>
+        <table>
+            <tr>
+                <td>회원번호</td>
+                <td>회원성명</td>
+                <td>전화번호</td>
+                <td>주소</td>
+                <td>가입일자</td>
+                <td>고객등급</td>
+                <td>거주지역</td>
+            </tr>
+    <%
+    request.setCharacterEncoding("UTF-8");
+    Connection conn = null;
+    Statement stmt = null;
+    String grade="";
+    try {
+        conn = Util.getConnection();
+        stmt = conn.createStatement();
+        String sql = "select * from tbl_member_202205 order by custno";
+        ResultSet rs = stmt.executeQuery(sql);
+        while(rs.next()) {
+            grade = rs.getString("grade");
+            switch(grade) {
+            case "A":
+                grade = "VIP";
+                break;
+            case "B":
+                grade = "일반";
+                break;
+            case "C":
+                grade = "직원";
+                break;
+            }
+    %>
+            <tr>
+            
+                <td><%=rs.getString("custno") %> </td>
+                <td><%=rs.getString("custname") %></td>
+                <td><%=rs.getString("phone") %></td>
+                <td><%=rs.getString("address") %></td>
+                <td><%=rs.getDate("joindate") %></td>
+                <td><%=grade %></td>
+                <td><%=rs.getString("city") %></td>
+            </tr>
+    <%
+        }
+    } catch(Exception e) {
+        e.printStackTrace();
+    }
+    %>
+        </table>
+    </form>
+    </section>
 
-s_sock = socket(AF_INET, SOCK_DGRAM)
-s_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-s_sock.bind(s_addr)
-
-data, c_addr = s_sock.recvfrom(1024)
-
-while True:
-    if data.decode('utf-8') == 'q':
-        break
-    print('Received from', c_addr)
-    print('수신된 자료', data.decode('utf-8'))
-    print() # enter
-    s_sock.sendto(data, c_addr)
-    data, c_addr = s_sock.recvfrom(1024)
-
-s_sock.close()
+    <jsp:include page="footer.jsp"/>
+    </body>
+    </html>
