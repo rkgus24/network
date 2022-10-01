1bfeb641d946ad506b040392ba44d1e78a8be89d
가현 <102145605+rkgus24@users.noreply.github.com>
2022-10-01 15:41:29 +0900

    Add files via upload

---
+++ Chapter23.py
@@ -0,0 +1,24 @@
+# UDPEchoClient
+
+from socket import *
+
+s_ip = 'localhost'
+s_port = 9999
+s_addr = (s_ip, s_port)
+
+c_sock = socket(AF_INET, SOCK_DGRAM)
+
+indata = input('전송할 문자열 입력 : ')
+
+c_sock.sendto(indata.encode('utf-8'), s_addr)
+
+while True:
+    if indata == 'q':
+        break
+    data, addr = c_sock.recvfrom(1024)
+    print('Echoed data : ', data.decode('utf-8'))
+    
+    indata = input('전송할 문자열 입력 : ')
+    c_sock.sendto(indata.encode('utf-8'), s_addr)
+
+c_sock.close()
